e6b179900c2025e8fedd5f4b92b61edd3768b4c4
ZishJawaid <zisheanj@gmail.com>
2020-08-19 14:19:20 +0100

    Added test for entering players

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -3,4 +3,14 @@ feature 'Testing infrastructure' do
       visit('/')
       expect(page).to have_content 'Testing infrastructure working!'
     end
+ end
+
+feature 'Entering player names' do
+  scenario 'players can enter names and see names on screen'
+    visit('/')
+    fill_in('First Name' , with: 'Zish')
+    click_button 'Send'
+    expect(page).to have_content 'Zish'
   end
+end
+

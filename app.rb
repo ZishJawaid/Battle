9b7d98a5d2583fe8d1dae2c4335d167edde10bb7
ZishJawaid <zisheanj@gmail.com>
2020-08-19 17:52:59 +0100

    Refactor: post redirect loop

--- app.rb
+++ app.rb
@@ -1,25 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
- 
-  enable :sessions
+
+    enable :sessions
 
     get '/' do
         erb(:index)
     end
 
     post '/names' do
-      session[:player1] = params[:player1]
-      session[:player2] = params[:player2]
-      redirect "/play"
+      session[:player_1] = params[:player_1]
+      session[:player_2] = params[:player_2]
+      redirect '/play'
     end
 
-    get '/play' do 
-      @player1 = session[:player1]
-      @player2 = session[:player2]
-      erb(:play)
+    get '/play' do
+     @player_1 = session[:player_1]
+     @player_2 = session[:player_2]
+     erb(:play)
     end
-
 
 run! if app_file == $0
 
--- spec/features/testing_entering_players_spec.rb
+++ spec/features/testing_entering_players_spec.rb
@@ -1,9 +1,9 @@
-feature 'Entering player names' do
-    scenario 'players can enter names and see names on screen' do
-      visit('/')
-      fill_in('player1' , with: 'Zish')
-      fill_in('player2' , with: 'Iliyan')
-      click_button 'Submit'
-      expect(page).to have_content 'Zish Iliyan'
-    end
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1, with: 'Zish'
+    fill_in :player_2, with: 'Sophia'
+    click_button 'Submit'
+    expect(page).to have_content 'Zish vs Sophia'
   end
+end

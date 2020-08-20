4cb62e4195875cf9e0821a2d9c610aa8b87c75dc
ZishJawaid <zisheanj@gmail.com>
2020-08-20 17:56:51 +0100

    Refactor: game class to instantiate with players and remove some global variables

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,18 @@
 require 'game'
 
 describe Game do
-    subject(:game) { described_class.new }
+    subject(:game) { described_class.new(player_1, player_2) }
     let(:player_1) { double "zish"}
     let(:player_2) { double "sophia" }
 
+    it 'intializes with a player 1' do
+        expect(game.player_1).to eq(player_1)
+    end
+
+    it 'intializes with a player 2' do
+        expect(game.player_2).to eq(player_2)
+    end
+    
     describe '#attack' do
         it 'attacks another player' do
             expect(player_2).to receive(:damage)

da170ebf7c8c4541e6801f094e9f7e7bb1b32293
ZishJawaid <zisheanj@gmail.com>
2020-08-20 23:19:02 +0100

    Added tests and implementation for multiplayer

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -1,9 +1,17 @@
 feature 'attack player' do
 
-    scenario 'attack player 2 and get confirmation' do
+    scenario 'player 1 attacks player 2 and get confirmation' do
         sign_in_and_play
         click_button('Attack')
         expect(page).to have_content 'Zish attacked Kibu!'
+    end
+
+    scenario 'player 2 attacks player 1 and get confirmation' do '
+        sign_in_and_play
+        click_button('Attack')
+        expect(page).to have_content 'Zish attacked Kibu!'
+        click_button('Attack')
+        expect(page).to have_content 'Kibu attacked Zish!'
     end
 
     scenario 'reduce player 2 HP by 10' do
--- spec/features/viewing_hit_points_spec.rb
+++ spec/features/viewing_hit_points_spec.rb
@@ -1,6 +1,11 @@
 feature 'view hit points' do
     scenario 'see player 2 hit points' do
         sign_in_and_play
-        expect(page).to have_content 'Kibu: received 20 hit points'
+        expect(page).to have_content 'Kibu: 100 hit points'
+    end
+
+    scenario 'see player 1 hit points' do
+        sign_in_and_play
+        expect(page).to have_content 'Zish: 100 hit points'
     end
 end
